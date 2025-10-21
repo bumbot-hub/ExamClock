63b4ef812262904497e956c6ac017a7c5a01eae4
K_Kamil <kkamil.kula@gmail.com>
2025-10-21 13:18:00 +0200

    chore: implemented homepage view

---
+++ main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron');
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        autoHideMenuBar: true
+    });
+
+    win.maximize();
+
+    win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
---
+++ renderer.js
@@ -0,0 +1,5 @@
+const contrast_btn = document.getElementById("contrast");
+
+contrast_btn.addEventListener("click", () => {
+    document.body.classList.toggle("high_contrast");
+})
