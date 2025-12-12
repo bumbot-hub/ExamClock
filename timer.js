d88687db7aa5c1f74750fd5973017a5cc5eb0253
bumbot-hub <kkamil.kula@gmail.com>
2025-12-12 20:44:14 +0100

    fix: bug fix

--- timer.js
+++ timer.js
@@ -1,67 +1,69 @@
 let intervalId = null;
 let remainingTime = 0;
+let fullTime = 0;
 
-export function startTimer(countdown, HTML, progress_bar){
-    let fullTime = countdown.slice(0,2)*60*60 + countdown.slice(3,5)*60 + countdown.slice(6,8);
-    let remainingTime = fullTime;
+function renderTime(HTML) {
+    const h   = Math.floor(remainingTime / 3600);
+    const min = Math.floor((remainingTime % 3600) / 60);
+    const s   = remainingTime % 60;
 
-    if(intervalId) clearInterval(intervalId);
+    HTML.innerHTML = [
+        h.toString().padStart(2, '0'),
+        min.toString().padStart(2, '0'),
+        s.toString().padStart(2, '0')
+    ].join(':');
+}
+
+function startInterval(HTML, progress_bar) {
+    if (intervalId) clearInterval(intervalId);
 
     intervalId = setInterval(() => {
-        if(remainingTime <= 0){
+        if (remainingTime < 0) {
             clearInterval(intervalId);
             intervalId = null;
+            remainingTime = 0;
             progress_bar.value = 100;
-        }else{
-            progress_bar.value = (remainingTime/fullTime)*100;
-
-            let h = Math.floor(remainingTime / 3600);
-            let min = Math.floor((remainingTime % 3600) / 60);
-            let s = remainingTime % 60;
-
-            h = h.toString().padStart(2, '0');
-            min = min.toString().padStart(2, '0');
-            s = s.toString().padStart(2, '0');
-
-            HTML.innerHTML = `${h}:${min}:${s}`;
-            remainingTime --;
+            HTML.innerHTML = '00:00:00';
+            endTimer();
+        } else {
+            progress_bar.value = (remainingTime / fullTime) * 100;
+            renderTime(HTML);
+            remainingTime--;
         }
     }, 1000);
 }
 
+export function startTimer(countdown, HTML, progress_bar){
+    fullTime =
+        parseInt(countdown.slice(0,2), 10) * 3600 +
+        parseInt(countdown.slice(3,5), 10) * 60;
+
+    remainingTime = fullTime;
+    renderTime(HTML);
+    startInterval(HTML, progress_bar);
+}
+
 export function pauseTimer(){
-    if(intervalId){
+    if (intervalId) {
         clearInterval(intervalId);
         intervalId = null;
     }
 }
 
-export function resumeTimer(HTML){
-    if(!intervalId && remainingTime > 0){
-        intervalId = setInterval(() => {
-            if(remainingTime <= 0){
-                clearInterval(intervalId);
-                intervalId = null;
-            } else {
-                let h = Math.floor(remainingTime / 3600);
-                let min = Math.floor((remainingTime % 3600) / 60);
-                let s = remainingTime % 60;
-
-                h = h.toString().padStart(2,'0');
-                min = min.toString().padStart(2,'0');
-                s = s.toString().padStart(2,'0');
-
-                HTML.innerHTML = `${h}:${min}:${s}`;
-                remainingTime--;
-            }
-        }, 1000);
+export function resumeTimer(HTML, progress_bar){
+    if (!intervalId && remainingTime > 0) {
+        startInterval(HTML, progress_bar);
     }
 }
 
 export function stopTimer(){
-    if(intervalId){
+    if (intervalId) {
         clearInterval(intervalId);
         intervalId = null;
-        remainingTime = 0;
     }
+    remainingTime = 0;
+}
+
+function endTimer(){
+
 }
